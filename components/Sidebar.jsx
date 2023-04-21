b2627008ddc7fce28cf1c28909ad40abbd05ff74
Nevil <nevilsaspara@gmail.com>
2023-04-21 14:03:30 +0530

    init

---
+++ components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {useState} from "react";
+import {
+  AiOutlineDashboard,
+  AiOutlineSetting,
+  AiOutlineUnorderedList,
+} from "react-icons/ai";
+import {BiHelpCircle} from "react-icons/bi";
+import {TbWorld, TbFiles} from "react-icons/tb";
+import {RiUserSearchLine, RiGalleryLine} from "react-icons/ri";
+import {HiOutlineDocumentDuplicate} from "react-icons/hi";
+import {TbLogout} from "react-icons/tb";
+import {MdLibraryBooks, MdOutlineSettingsApplications} from "react-icons/md";
+
+const Sidebar = () => {
+  return <>hi</>;
+};
+
+export default Sidebar;
